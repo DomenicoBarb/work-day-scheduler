8167962c323df6b1a30ab32daaf4912cb5a3f6f7
DomenicoBarb <domenicobarberi585@gmail.com>
2023-01-22 22:46:40 -0500

    Added vars for current day/hour, added local storage functionality, added js to change cell colours to appropriate state (past, present, future) and added a message displaying that data has been saved locally.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,23 +1,53 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
+//Display Current Date in Header
+var currentDay = dayjs().format('dddd, MMMM, D, hh:mm A');
+$('#currentDay').text(currentDay); 
+//Find Current Hour
+var currentHour = dayjs().hour();
+$('#currentHour').text(currentHour); 
+
+// utilizing document.ready to make sure HTMl is fully loaded
+$(document).ready(function () {
+  // when saveBtn is click
+  $(".saveBtn").click(function(){
+    // test alert for debugging alert("Button was clicked!");
+    // key creation (what hour was selected) (IE location)
+    var key = $(this).parent().attr("id").split("-")[1];
+    // value creation (what was typed into the text aread) (content inputtedadd)
+    var value = $(this).parent().find(".description").val();
+    // save key and value to the local storage
+    localStorage.setItem(key,value);
+    // data saved to local storage message code
+    var parentP = document.getElementById("dataSaved");
+    var childP = document.createElement("p");
+    // insert "data save to local storage!" into the existing <p> with id="dataSaved"
+    childP.innerHTML = "Data saved to local storage! ✔️";
+    parentP.appendChild(childP);
+  });
+
+  // text area formatting
+$(".time-block").each(function(){
+  // obtain hour of each cell using 24 hour formatting, removing "hour-" and obtaining that cells hour (7-17, meaning 7pm tp 5pm)
+  var blockHour = $(this).attr("id").split("-")[1];
+  // retrieves local storage data and displays it in appropriate cell
+  var textEntry = localStorage.getItem(blockHour);
+  var textArea = $(this).find(".description");
+  textArea.val (textEntry);
+  // if statements to change time slots to past, present, or future by comparing the block time to current time
+  if (blockHour < currentHour){
+      // adds the class of past to appropriate cell(s)
+      $(this).find(".description").addClass("past");
+      // console log how many cells are past
+      console.log("past");
+  }else if(blockHour == currentHour){
+      // adds the class of present to the appropriate cell
+      $(this).find(".description").addClass("present");
+      // console log how many cells are present (should only ever be ONE)
+      console.log("present");
+  }else{
+      // adds the class of future to the appropriate cell(s)
+      $(this).find(".description").addClass("future");
+      // console log how many cells are future
+      console.log("future");
+   }
+});
 });
